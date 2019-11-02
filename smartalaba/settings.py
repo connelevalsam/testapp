17740c997fbe023c9701b377650b453ff4b227bc
connelevalsam <connelblaze@gmail.com>
2019-11-02 18:25:49 +0100

    updated setting file

--- smartalaba/settings.py
+++ smartalaba/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = S3Connection(os.environ['SMART_KEY'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://smartalabatest.herokuapp.com/']
 
 # Application definition
 
