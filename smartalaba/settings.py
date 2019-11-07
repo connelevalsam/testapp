b65801eb41d961b38f6981fe82ba5be2377df83e
connelevalsam <connelblaze@gmail.com>
2019-11-07 10:38:24 +0100

    updated setting file

--- smartalaba/settings.py
+++ smartalaba/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/pages/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
